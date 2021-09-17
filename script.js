631ef7a9a949eba48bae0e76a9c3f5736261c31e
makosso <makossominion@gmail.com>
2021-09-17 14:34:33 +0200

    bonjour

--- script.js
+++ script.js
@@ -1,28 +1,5 @@
-const num1 = 100;
-const num2 = 50;
-let val;
-
-// CALCUL SIMPLE:
-val = num1 + num2;
-val = num1 * num2;
-val = num1 - num2;
-val = num1 / num2;
-val = num1 % num2;
-
-// LES FONCTIONS  ARITHMETIQUES
-val = Math.PI;
-val = Math.round(2.4);
-val = Math.ceil(2.4);
-console.log(val);
-val = Math.floor(2.8);
-val = Math.abs(0);
-val = Math.sign(23.5)
-//  RETOURNE
-val = Math.ceil(2.3);
-// RETOURNE 
-    val = Math.floor(9.5);
-    val =  Math.max(2,6);
-    val = Math.sqrt(64);
-    val = Math.pow(2,2);
-    val = Math.random()*2;
-console.log(val);     
+let x;
+let x;
+x = 12;
+y = 3;
+console.log(toString(x));
