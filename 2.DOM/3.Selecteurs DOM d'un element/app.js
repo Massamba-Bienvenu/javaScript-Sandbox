26d19652402a3c68c35e735b488c7d1fa8d524fb
Massamba-Bienvenu <boukamass@gmail.com>
2021-10-13 14:30:06 -0500

    finition de querySelector

--- 2.DOM/3.Selecteurs DOM d'un element/app.js
+++ 2.DOM/3.Selecteurs DOM d'un element/app.js
@@ -16,22 +16,18 @@
 // // Modifier le contenu
 // taskTitle.textContent = 'Task List';
 // taskTitle.innerText = 'My Tasks';
-// taskTitle.innerHTML = '<span style="color:red">Task List</span>';
+// taskTitle.innerHTML = '<span style="color:red">Task List</span>'; inserer un html
 
 //METHODE 2 document.querySelector()**BEST**
-console.log(document.querySelector('#task-title'));
-console.log(document.querySelector('.card-title'));
-console.log(document.querySelector('h5'));
+console.log(document.querySelector("#task-title"));
+console.log(document.querySelector(".card-title"));
+console.log(document.querySelector("h5"));
 
-document.querySelector('li').style.color = 'red';
-document.querySelector('ul li').style.color = 'blue';
+document.querySelector("li").style.color = "red";
+document.querySelector("ul li").style.color = "blue";
 
-document.querySelector('li:last-child').style.color = 'red';
-document.querySelector('li:nth-child(3)').style.color = 'yellow';
-document.querySelector('li:nth-child(4)').textContent = 'Hello World';
-document.querySelector('li:nth-child(odd)').style.background = '#ccc';
-document.querySelector('li:nth-child(even)').style.background = '#f4f4f4';
-
-
-
-
+document.querySelector("li:last-child").style.color = "red";
+document.querySelector("li:nth-child(3)").style.color = "yellow";
+document.querySelector("li:nth-child(4)").textContent = "Hello World";
+document.querySelector("li:nth-child(odd)").style.background = "#ccc";
+document.querySelector("li:nth-child(even)").style.background = "#f4f4f4";
--- 2.DOM/3.Selecteurs DOM d'un element/test.js
+++ 2.DOM/3.Selecteurs DOM d'un element/test.js
@@ -1,2 +1,12 @@
-// METHODE 1: document.getElementById()
-console.log(document.getElementById("task-title"));
+console.log(document.querySelector("#task-title"));
+console.log(document.querySelector(".card-title"));
+console.log(document.querySelector("h5"));
+
+document.querySelector("li").style.color = "red";
+document.querySelector("ul li").style.color = "blue";
+
+document.querySelector("li:last-child").style.color = "red";
+document.querySelector("li:nth-child(3)").style.color = "Yellow";
+document.querySelector("li:nth-child(4)").textContent = "John smith";
+document.querySelector("li:nth-child(odd)").style.background = "#ccc";
+document.querySelector("li:nth-child(even)").style.background = "#f4f4f4";
